476fa9c7ca02c1c0d74ef2be894e4288a9f98f5f
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2018-08-15 21:38:58 -0400

    added getSong function

--- liri.js
+++ liri.js
@@ -1,22 +1,20 @@
 require('dotenv').config();
 var keys = require('./keys.js');
 var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
 var request = require('request');
 
 var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+var client = new Twitter(keys.twitter);
 
 var expr = process.argv[2];
 
 switch (expr) {
   case 'my-tweets':
     getTweets();
-    console.log('hi');
     break;
   case 'spotify-this-song':
-    var songName = process.argv[3];
     getSong();
-    console.log(songName);
     break;
   case 'movie-this':
     getMovie();
@@ -27,8 +25,41 @@ switch (expr) {
     break;
 }
 
-function getTweets() {}
-function getSong() {}
+function getTweets() {
+  var params = { q: '#nodejs', count: 20, result_type: 'recent', lang: 'en' };
+  client.get('statuses/user_timeline', params, function(
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      for (tweet in tweets) {
+        console.log(
+          `----------------------- \n${tweets[tweet].created_at}: ${
+            tweets[tweet].text
+          }\n`
+        );
+      }
+    }
+  });
+}
+
+function getSong() {
+  var songName = process.argv[3];
+  if (songName === undefined) {
+    songName = "The Sign";
+  } else {
+    songName = songName.split(' ').join('+');
+  }
+  spotify.search({ type: 'track', query: songName, limit:1 }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(JSON.stringify(data)); 
+  });
+}
+
 function getMovie() {
   var movieName = process.argv[3];
   if (movieName === undefined) {
@@ -38,26 +69,33 @@ function getMovie() {
   }
 
   var queryUrl =
-    'http://www.omdbapi.com/?t=' + movieName + '&y=&plot=short&apikey='+process.env.OMDB_API_KEY;
+    'http://www.omdbapi.com/?t=' +
+    movieName +
+    '&y=&plot=short&apikey=' +
+    process.env.OMDB_API_KEY;
   console.log(queryUrl);
   request(queryUrl, function(error, response, body) {
     // If the request is successful
     // error first callback
     if (!error && response.statusCode === 200) {
-      var movieData = JSON.parse(body)
+      var movieData = JSON.parse(body);
 
-      // Parse the body of the site and recover just the imdbRating
-      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-      // console.log(response)
-      console.log(`Title: ${movieData.Title}`);
-      console.log(`Year of release: ${movieData.Released.split(' ')[2]}`);
-      console.log(`IMDB Rating: ${movieData.imdbRating}`);
-      console.log(`Rotten Tomatoes Rating: ${movieData.Ratings[1].Source}`);
-      console.log(`Country: ${movieData.Country}`);
-      console.log(`Language: ${movieData.Language}`);
-      console.log(`Plot: ${movieData.Plot}`);
-      console.log(`Actors: ${movieData.Actors}`);
-      console.log('Release Year: ' + JSON.parse(body).Year);
+      // Parse the body of the site
+      var data = {
+        Title: movieData.Title,
+        'Year of release': movieData.Released.split(' ')[2],
+        'IMDB Rating': movieData.imdbRating,
+        'Rotten Tomatoes Rating': movieData.Ratings[1].Source,
+        Country: movieData.Country,
+        Language: movieData.Language,
+        Plot: movieData.Plot,
+        Actors: movieData.Actors
+      };
+
+      // display data
+      for (var item in data) {
+        console.log(`${item}: ${data[item]}`);
+      }
     }
   });
 }
